37bd1584feb7d880364add33b5eafc86b07cc78e
geedha k <geedha.india@gmail.com>
2020-04-05 12:10:23 +0530

    done

--- src/main/java/com/geedha/web/Inserstudent.java
+++ src/main/java/com/geedha/web/Inserstudent.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 import com.google.gson.Gson;
 import com.mysql.cj.jdbc.MysqlDataSource;
-@WebServlet("\studentadd")
+@WebServlet("/studentadd")
 public class Inserstudent extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
