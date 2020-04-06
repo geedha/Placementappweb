76e9ed91bebd2a6ffd35fbed26829689dc0226c1
geedha k <geedha.india@gmail.com>
2020-04-06 14:23:56 +0530

    done

---
+++ src/main/java/com/geedha/web/Company.java
@@ -0,0 +1,46 @@
+package com.geedha.web;
+public class Company {
+    private String name;
+    private String email;
+    private String password;
+    
+
+    public Company(){
+
+    }
+    public Company(String name, String email,String password)
+    {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+       
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getEmail()
+    {
+        return this.email;
+    }
+    public void setEmail(String email)
+    {
+            this.email = email;
+    }
+    public String  getPassword()
+    {
+        return this.password;
+
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+   
+
+    
+
+}
---
+++ src/main/java/com/geedha/web/Insertcompany.java
@@ -0,0 +1,69 @@
+package com.geedha.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+@WebServlet("/companyadd")
+public class Insertcompany extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw = resp.getWriter();
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://remotemysql.com:3306/Ykiry88Nqj");
+        dataSource.setUser("Ykiry88Nqj");
+        dataSource.setPassword("82aXSBlvtP");
+       /* String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");*/
+        
+    
+       
+        
+        String name = "haaii";
+        String email  = "dsd";
+        String password = "434fdf";
+       
+        
+            
+        try{
+        
+            Connection con=dataSource.getConnection();
+            System.out.println("Connection Ess");
+            System.out.println("inte");
+            //here sonoo is database name, root is username and password  
+            Statement stmt=con.createStatement();
+
+            stmt.executeUpdate("insert into  company values ('"+name+"','"+email+"', '"+password+"')"); 
+            
+            System.out.println("Queeeee");
+            
+
+            pw.println("Done");
+        }
+        catch(Exception e){
+
+
+
+        }
+    
+    }
+    
+}
+    
+
+
